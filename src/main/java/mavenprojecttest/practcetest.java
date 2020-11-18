8afc6c08d355530fac6a037b83f5454a11245257
shaheenarahman <shaheenarahman@shaheenas-mbp.lan>
2020-11-17 19:36:16 -0500

    this is a new maven project

---
+++ src/main/java/mavenprojecttest/practcetest.java
@@ -0,0 +1,10 @@
+package mavenprojecttest;
+
+public class practcetest {
+
+	public static void main(String[] args) {
+		System.out.println("I LOVE TO LEARN JAVA ");
+
+	}
+
+}
---
+++ src/main/java/mavenprojecttest/practicejava2.java
@@ -0,0 +1,10 @@
+package mavenprojecttest;
+
+public class practicejava2 {
+
+	public static void main(String[] args) {
+	System.out.println("New York : I love you");
+
+	}
+
+}
